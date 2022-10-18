20ac3741451730c505a77dfa06c74b0c2ece4649
duyme18 <duyme18@gmail.com>
2022-10-18 17:28:24 +0700

    todoList

--- src/App.js
+++ src/App.js
@@ -1,14 +1,51 @@
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [job, setJob] = useState('')
+  const [jobs, setJobs] = useState(() => {
+    const storageJobs = JSON.parse(localStorage.getItem('jobs'))
+    return storageJobs ?? []
+  })
+
+  const handleSubmit = () => {
+    setJobs(prev => {
+      const newJobs = [...prev, job]
+      if (job) {
+        const jsonJobs = JSON.stringify(newJobs)
+        localStorage.setItem('jobs', jsonJobs)
+        return newJobs
+      } else {
+        alert('job not found')
+        return prev
+      }
+    })
+    setJob('')
+  }
+
+  const handleDelete = (index) => {
+    setJobs(job => {
+      const newJobs = [...job]
+      newJobs.splice(index, 1)
+      const jsonJobs = JSON.stringify(newJobs)
+      localStorage.setItem('jobs', jsonJobs)
+      return newJobs
+    })
+  }
+
   return (
     <div className="App">
-      <input />
-      <button>Add</button>
+      <input
+        value={job}
+        onChange={e => setJob(e.target.value)}
+      />
+      <button onClick={handleSubmit}>Add</button>
       <ul>
-        <li>
-          
-        </li>
+        {jobs?.map((job, index) => (
+          <li key={index}>
+            {job}
+            <button onClick={() => handleDelete(index)}>Delete</button>
+          </li>
+        ))}
       </ul>
     </div>
   );
