eb927db63c731931d93e17f6a6434a0cf389a809
duyhd <duyhd@gemvietnam.com>
2022-10-18 22:57:22 +0700

    1 2 use effect

--- src/Content.js
+++ src/Content.js
@@ -1,8 +1,46 @@
+import { useEffect, useState } from "react"
+
+
+
+// 1. useEffect(callback)
+// - goi callback moi khi component re-render
+// - goi callback sau khi component them element vao DOM
+// 2. useEffect(callback, [])
+// - chi goi callback sau khi component mounted
+// 3. useEffect(callback, [deps])
+
+// ------------
+// 1. callback luon duoc goi sau khi component mounted
 
 function Content() {
+
+    // 1. useEffect(callback)
+
+    const [title, setTitle] = useState('')
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(res => res.json())
+            .then(posts => {
+                setPosts(posts)
+            })
+    }, [])
+
+
+
     return (
         <div>
-            <h1>Hello World</h1>
+            <input
+                value={title}
+                onChange={e => setTitle(e.target.value)}
+            />
+            <ul>
+                {posts.map(post =>
+                    <li key={post.id}>
+                        {post.title}
+                    </li>)}
+            </ul>
         </div>
     )
 }
