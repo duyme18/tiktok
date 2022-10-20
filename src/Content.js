da97b546911dc10ea059607436878dab0b2369f8
duyme18 <duyme18@gmail.com>
2022-10-20 14:27:31 +0700

    srcoll

--- src/Content.js
+++ src/Content.js
@@ -12,6 +12,19 @@ import { useEffect, useState } from "react"
 
 // ------------
 // 1. callback luon duoc goi sau khi component mounted
+// 2. cleanup function luôn được gọi trước khi component unmounted
+
+/**
+ * 1. Update DOM
+ * 2. Call api
+ * 3. listen DOM events
+ * - scroll
+ * - resize
+ * 4. clean up
+ * - remove listener/ unsubscribe
+ * - clea timers
+ * 
+ */
 
 const tabs = ['posts', 'comments', 'albums']
 
@@ -22,6 +35,7 @@ function Content() {
     const [title, setTitle] = useState('')
     const [posts, setPosts] = useState([])
     const [type, setType] = useState('posts')
+    const [showGoToTop, setShowGoToTop] = useState(false)
 
     useEffect(() => {
         fetch(`https://jsonplaceholder.typicode.com/${type}`)
@@ -31,7 +45,22 @@ function Content() {
             })
     }, [type])
 
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY >= 200) {
+                setShowGoToTop(true)
+            } else {
+                setShowGoToTop(false)
+            }
+            // setShowGoToTop(window.scrollY >= 200)
+        }
+        window.addEventListener('scroll', handleScroll)
 
+        // cleanup function
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+        }
+    }, [])
 
     return (
         <div>
@@ -39,8 +68,8 @@ function Content() {
                 <button
                     key={tab}
                     style={type === tab ? {
-                        color:'#fff',
-                        backgroundColor:'#333',
+                        color: '#fff',
+                        backgroundColor: '#333',
                     } : {}}
                     onClick={() => {
                         setType(tab)
@@ -60,7 +89,19 @@ function Content() {
                         {post.title || post.name}
                     </li>)}
             </ul>
-        </div>
+            {showGoToTop && (
+                <button
+                    style={{
+                        position: 'fixed',
+                        right: 20,
+                        bottom: 20
+                    }}
+                >
+                    Go to Top
+                </button>
+            )
+            }
+        </div >
     )
 }
 
