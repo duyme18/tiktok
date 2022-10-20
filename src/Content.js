d1ff6d0476bd58a34ab1f61177c55ff480a1a1c5
duyme18 <duyme18@gmail.com>
2022-10-20 14:46:02 +0700

    resize

--- src/Content.js
+++ src/Content.js
@@ -1,8 +1,24 @@
+import { useEffect, useState } from "react"
 
 function Content() {
+
+    const [width, setWidth] = useState(window.innerWidth)
+
+    
+    useEffect(() => {
+        const handleResize = () => {
+            setWidth(window.innerWidth)
+        }
+        window.addEventListener('resize', handleResize)
+        return ()=>{
+            window.removeEventListener('resize', handleResize)
+        } 
+        
+        
+    },[])
     return (
         <div>
-            <h1>Hello World</h1>
+            <h1>{width}</h1>
         </div>
     )
 }
