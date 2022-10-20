faf107d66775b8e49f00b038f094c908f6f38a32
duyme18 <duyme18@gmail.com>
2022-10-20 15:05:02 +0700

    timer

--- src/Content.js
+++ src/Content.js
@@ -1,8 +1,19 @@
+import { useEffect, useState } from "react"
 
 function Content() {
+    const [countdown, setCountdown] = useState(180)
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setCountdown(prevState => prevState - 1)
+        }, 1000)
+
+        return () => clearInterval(timer)
+    }, [])
+
     return (
         <div>
-            <h1>Hello World</h1>
+            <h1>{countdown}</h1>
         </div>
     )
 }
