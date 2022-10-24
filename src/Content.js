6bdd5da0478cf46ffcf162bb224845f54de7ca4e
duyme18 <duyme18@gmail.com>
2022-10-24 15:56:26 +0700

    chat app

--- src/Content.js
+++ src/Content.js
@@ -1,8 +1,45 @@
+import { useEffect, useState } from "react"
 
+const lessons = [
+    {
+        id: 1,
+        name: 'Bai 1'
+    },
+    {
+        id: 2,
+        name: 'Bai 2'
+    },
+    {
+        id: 3,
+        name: 'Bai 3'
+    }
+]
 function Content() {
+    const [lessonId, setLessonId] = useState(1)
+
+    useEffect(() => {
+        const handleComment = ({detail}) => {
+            console.log(detail)
+        }
+        window.addEventListener(`lesson-${lessonId}`, handleComment)
+        return () => (
+            window.removeEventListener(`lesson-${lessonId}`, handleComment)
+        )
+    },[lessonId])
+
     return (
         <div>
-            <h1>Hello World</h1>
+            <ul>
+                {lessons.map(lesson => (
+                    <li
+                        key={lesson.id}
+                        style={{ color: lessonId === lesson.id ? 'red' : '#333' }}
+                        onClick={() => setLessonId(lesson.id)}
+                    >
+                        {lesson.name}
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
