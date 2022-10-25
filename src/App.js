d99b3e03dd0f92de632909a35328dacda4853e35
duyme18 <duyme18@gmail.com>
2022-10-25 09:08:40 +0700

    memo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import './App.css';
 import Content from './Content';
-import { useState } from 'react';
+import { useState, memo } from 'react';
+
+// 1. memo() -> higher order component (HOC)
+// ghi nhớ lại những props của component để quyết định có reder lại component để tối ưu hiệu năng
+// 2. useCallback()
+
+// Hooks
+// HOC
+// Render props
 
 function App() {
-  const [show, setShow] = useState(false)
+  const [count, setCount] = useState(0)
+
+  const increse = () => {
+    setCount(count + 1)
+  }
   return (
-    <div className="App">
-      <button onClick={() => setShow(!show)}>Toggle</button>
-      {show && <Content />}
+    <div className="App" style={{ padding: 20 }}>
+      <Content />
+      <h1>{count}</h1>
+      <button onClick={increse}>Click me</button>
     </div>
   );
 }
--- src/Content.js
+++ src/Content.js
@@ -1,47 +1,13 @@
-import { useEffect, useState } from "react"
+import { memo, useState } from "react"
 
-const lessons = [
-    {
-        id: 1,
-        name: 'Bai 1'
-    },
-    {
-        id: 2,
-        name: 'Bai 2'
-    },
-    {
-        id: 3,
-        name: 'Bai 3'
-    }
-]
+
 function Content() {
-    const [lessonId, setLessonId] = useState(1)
-
-    useEffect(() => {
-        const handleComment = ({detail}) => {
-            console.log(detail)
-        }
-        window.addEventListener(`lesson-${lessonId}`, handleComment)
-        return () => (
-            window.removeEventListener(`lesson-${lessonId}`, handleComment)
-        )
-    },[lessonId])
 
     return (
         <div>
-            <ul>
-                {lessons.map(lesson => (
-                    <li
-                        key={lesson.id}
-                        style={{ color: lessonId === lesson.id ? 'red' : '#333' }}
-                        onClick={() => setLessonId(lesson.id)}
-                    >
-                        {lesson.name}
-                    </li>
-                ))}
-            </ul>
+           <h2>Hello</h2>
         </div>
     )
 }
 
-export default Content
+export default memo(Content)
